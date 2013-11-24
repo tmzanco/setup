ec257a5d48d0c9970317c300686e810e0b260fa3
Tal Ziv <talziv@gmail.com>
2013-11-23 23:32:35 -0500

    custom

--- setup.sh
+++ setup.sh
@@ -2,24 +2,10 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
+sudo apt-get -y update
+
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
-
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
-
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
@@ -27,22 +13,38 @@ sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
-# Install Heroku toolbelt
-# https://toolbelt.heroku.com/debian
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+sudo apt-get install -y mysql-server
+
+# python / django / postgres connectors
+sudo apt-get install -y python-django
+sudo apt-get install -y python-psycopg2
+
+wget raw.github.com/startup-class/dotfiles/master/.screenrc -O .screenrc
+sudo apt-get install -y mercurial
+
+# for email
+sudo apt-get install -y postfix
+
+# for rss
+sudo pip install -y feedparser
+
+# for html parsing
+sudo apt-get install -y python-bs4
+
+
 
 # git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+# cd $HOME
+# if [ -d ./dotfiles/ ]; then
+#     mv dotfiles dotfiles.old
+# fi
+# if [ -d .emacs.d/ ]; then
+#     mv .emacs.d .emacs.d~
+# fi
+# git clone https://github.com/startup-class/dotfiles.git
+# ln -sb dotfiles/.screenrc .
+# ln -sb dotfiles/.bash_profile .
+# ln -sb dotfiles/.bashrc .
+# ln -sb dotfiles/.bashrc_custom .
+# ln -sf dotfiles/.emacs.d .
 
